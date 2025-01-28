b8e1270f36a0149bd1402738e7bba92b80844adf
Nate Kelley <nate@buster.so>
2025-01-28 10:07:37 -0700

    Update ChatResponseMessage_Thought.tsx

--- web/src/app/app/_layouts/ChatLayout/ChatContainer/ChatContent/ChatResponseMessages/ChatResponseMessage_Thought.tsx
+++ web/src/app/app/_layouts/ChatLayout/ChatContainer/ChatContent/ChatResponseMessages/ChatResponseMessage_Thought.tsx
@@ -1,9 +1,16 @@
-import { BusterChatMessageResponse } from '@/api/buster_socket/chats';
+import { BusterChatMessage_thought, BusterChatMessageResponse } from '@/api/buster_socket/chats';
 import React from 'react';
 import { ChatResponseMessageProps } from './ChatResponseMessages';
+import { AnimatePresence, motion } from 'framer-motion';
+import { animationConfig } from './animationConfig';
+import { CircleSpinnerLoader } from '@/components/loaders/CircleSpinnerLoader';
+import { Text } from '@/components/text';
 
 export const ChatResponseMessage_Thought: React.FC<ChatResponseMessageProps> = React.memo(
-  ({ responseMessage }) => {
+  ({ responseMessage: responseMessageProp }) => {
+    const responseMessage = responseMessageProp as BusterChatMessage_thought;
+    const { thought_title, thought_secondary_title, thought_pill, in_progress } = responseMessage;
+
     return <div>ChatResponseMessage_Thought</div>;
   }
 );
