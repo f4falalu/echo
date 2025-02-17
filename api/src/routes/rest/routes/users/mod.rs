bcf242824c0279ab5481a14048bd399c344d0305
dal <dallin@buster.so>
2025-02-17 10:03:05 -0700

    move user back into authed endpoint

--- api/src/routes/rest/routes/users/mod.rs
+++ api/src/routes/rest/routes/users/mod.rs
@@ -1,10 +1,7 @@
 use axum::{
-    middleware,
     routing::{get, put},
     Router,
 };
-
-use crate::buster_middleware::auth::auth;
 
 mod assets;
 mod get_user;
@@ -14,11 +11,7 @@ mod update_user;
 pub fn router() -> Router {
     Router::new()
         .route("/", get(get_user::get_user))
-        .merge(
-            Router::new()
-                .route("/:user_id", put(update_user::update_user))
-                .route("/:user_id", get(get_user_by_id::get_user_by_id))
-                .nest("/:user_id", assets::router())
-                .route_layer(middleware::from_fn(auth))
-        )
+        .route("/:user_id", put(update_user::update_user))
+        .route("/:user_id", get(get_user_by_id::get_user_by_id))
+        .nest("/:user_id", assets::router())
 }
