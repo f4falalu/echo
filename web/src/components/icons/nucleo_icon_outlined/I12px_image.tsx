3f014c89c01ddae2e9ad4d841d95d02d809bba3a
Nate Kelley <nate@buster.so>
2025-02-18 11:00:49 -0700

    update icons

--- web/src/components/icons/nucleo_icon_outlined/I12px_image.tsx
+++ web/src/components/icons/nucleo_icon_outlined/I12px_image.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import { iconProps } from './iconProps';
 
+function I12px_image(props: iconProps) {
+  const fill = props.fill || 'currentColor';
+  const secondaryfill = props.secondaryfill || fill;
+  const strokewidth = props.strokewidth || 1;
+  const width = props.width || '1em';
+  const height = props.height || '1em';
+  const title = props.title || '12px image';
 
+  return (
+    <svg height={height} width={width} viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+      <title>{title}</title>
+      <g fill={fill}>
+        <path
+          d="m2.32,10.516l4.723-4.723c.391-.391,1.024-.391,1.414,0l2.293,2.293"
+          fill="none"
+          stroke={secondaryfill}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokewidth}
+        />
+        <circle cx="4" cy="4" fill={secondaryfill} r="1" strokeWidth="0" />
+        <rect
+          height="9.5"
+          width="9.5"
+          fill="none"
+          rx="2"
+          ry="2"
+          stroke={fill}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokewidth}
+          x="1.25"
+          y="1.25"
+        />
+      </g>
+    </svg>
+  );
+}
 
-function image(props: iconProps) {
-	const fill = props.fill || 'currentColor';
-	const secondaryfill = props.secondaryfill || fill;
-	const strokewidth = props.strokewidth || 1;
-	const width = props.width || '1em';
-	const height = props.height || '1em';
-	const title = props.title || "12px image";
-
-	return (
-		<svg height={height} width={width} viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-	<title>{title}</title>
-	<g fill={fill}>
-		<path d="m2.32,10.516l4.723-4.723c.391-.391,1.024-.391,1.414,0l2.293,2.293" fill="none" stroke={secondaryfill} strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokewidth}/>
-		<circle cx="4" cy="4" fill={secondaryfill} r="1" strokeWidth="0"/>
-		<rect height="9.5" width="9.5" fill="none" rx="2" ry="2" stroke={fill} strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokewidth} x="1.25" y="1.25"/>
-	</g>
-</svg>
-	);
-};
-
-export default image;
+export default I12px_image;
