da90476acbf995f3dc2063a5ad546342a1e88165
Nate Kelley <nate@buster.so>
2025-02-28 11:45:52 -0700

    buttons

--- web/src/layouts/ChatLayout/FileContainer/FileContainerHeader/MetricContainerHeaderButtons.tsx
+++ web/src/layouts/ChatLayout/FileContainer/FileContainerHeader/MetricContainerHeaderButtons.tsx
@@ -8,9 +8,9 @@ import { FileButtonContainer } from './FileButtonContainer';
 import { CreateChatButton } from './CreateChatButtont';
 import { SelectableButton } from './SelectableButton';
 import { useMetricIndividual } from '@/context/Metrics';
-import { SaveMetricToCollectionButton } from '../../../../components/features/button/SaveMetricToCollectionButton';
-import { SaveMetricToDashboardButton } from '../../../../components/features/button/SaveMetricToDashboardButton';
-import { ShareMetricButton } from '../../../../components/features/button/ShareMetricButton';
+import { SaveMetricToCollectionButton } from '../../../../components/features/buttons/SaveMetricToCollectionButton';
+import { SaveMetricToDashboardButton } from '../../../../components/features/buttons/SaveMetricToDashboardButton';
+import { ShareMetricButton } from '../../../../components/features/buttons/ShareMetricButton';
 
 export const MetricContainerHeaderButtons: React.FC<FileContainerButtonsProps> = React.memo(() => {
   const renderViewLayoutKey = useChatLayoutContextSelector((x) => x.renderViewLayoutKey);
