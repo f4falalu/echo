ebdd955533b1050199b751c5e3f52443ec7f6d1f
dal <dallin@buster.so>
2025-03-03 15:38:38 -0700

    ok we added in the delete endpoint for messages

---
+++ api/libs/handlers/src/messages/helpers/mod.rs
@@ -0,0 +1,3 @@
+pub mod delete_message_handler;
+
+pub use delete_message_handler::*;
---
+++ api/src/routes/rest/routes/messages/mod.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::{delete, get, post},
+    Router,
+};
+
+mod delete_message;
+
+use delete_message::delete_message_rest_handler;
+
+/// Create a router for message-related endpoints
+pub fn router() -> Router {
+    Router::new()
+        // Delete a message and all subsequent messages in the same chat
+        .route("/:id", delete(delete_message_rest_handler))
+} 
