2649fb7656eb4eea02a5d0df189c7f0fa70917bd
dal <dallin@buster.so>
2025-04-04 16:25:20 -0600

    last few utils to clean up

--- api/server/src/utils/clients/mod.rs
+++ api/server/src/utils/clients/mod.rs
@@ -1,6 +1,2 @@
-pub mod ai;
-// pub mod aws;
 pub mod email;
-pub mod posthog;
 pub mod sentry_utils;
-pub mod typesense;
--- api/server/src/utils/mod.rs
+++ api/server/src/utils/mod.rs
@@ -1,7 +1,5 @@
 pub mod clients;
 pub mod security;
-pub mod stored_values;
 
 pub use agents::*;
 pub use security::*;
-pub use stored_values::*;
