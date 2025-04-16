16ecff9041794a16d52fcf668d07ece6f692c160
Nate Kelley <nate@buster.so>
2025-04-16 15:46:58 -0600

    Update requests.ts

--- web/src/api/buster_rest/chats/requests.ts
+++ web/src/api/buster_rest/chats/requests.ts
@@ -64,7 +64,6 @@ export const updateChat = async ({
   return mainApi.put<BusterChat>(`${CHATS_BASE}/${id}`, data).then((res) => res.data);
 };
 
-export const deleteChat = async (data: string[]): Promise<void> => {
 export const updateChatMessageFeedback = async ({
   message_id,
   ...params
