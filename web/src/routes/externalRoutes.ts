f7f74d9e83d0d28227ac70650f0e91ea8dc8ddf9
Nate Kelley <nate@buster.so>
2025-04-21 15:01:17 -0600

    402 redirect page

--- web/src/routes/externalRoutes.ts
+++ web/src/routes/externalRoutes.ts
@@ -1,2 +1,3 @@
 export const BUSTER_HOME_PAGE = 'https://buster.so';
 export const BUSTER_DOCS_URL = 'https://docs.buster.so';
+export const BUSTER_GETTING_STARTED_URL = 'https://www.buster.so/get-started';
