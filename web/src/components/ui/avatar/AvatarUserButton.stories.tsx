6ba7b77de28059c37b918cde48cafa7db7820eb0
Nate Kelley <nate@buster.so>
2025-04-22 16:54:45 -0600

    Update AvatarUserButton.stories.tsx

--- web/src/components/ui/avatar/AvatarUserButton.stories.tsx
+++ web/src/components/ui/avatar/AvatarUserButton.stories.tsx
@@ -3,7 +3,8 @@ import { AvatarUserButton } from './AvatarUserButton';
 
 const defaultUser = {
   username: 'John Doe',
-  avatarUrl: 'https://i.pravatar.cc/200',
+  avatarUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=200&h=200&q=80',
   email: 'john.doe@example.com'
 };
 
