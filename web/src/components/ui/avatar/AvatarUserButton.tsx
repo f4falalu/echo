0504910e8ea0e5457ca47cf9a1dbdc5047787cfe
Nate Kelley <nate@buster.so>
2025-04-22 16:49:32 -0600

    truncate name cards

--- web/src/components/ui/avatar/AvatarUserButton.tsx
+++ web/src/components/ui/avatar/AvatarUserButton.tsx
@@ -16,9 +16,11 @@ export const AvatarUserButton = React.forwardRef<
       ref={ref}
       className="hover:bg-item-hover active:bg-item-active flex w-full cursor-pointer items-center gap-x-2 rounded-md p-2">
       <Avatar size={32} fallbackClassName="text-base" image={avatarUrl} name={username} />
-      <div className="flex flex-col gap-y-0.5">
-        <Text className="flex-grow">{username}</Text>
-        <Text size={'sm'} variant={'secondary'}>
+      <div className="flex w-full flex-col gap-y-0.5 overflow-hidden">
+        <Text truncate className="flex-grow">
+          {username}
+        </Text>
+        <Text truncate size={'sm'} variant={'secondary'}>
           {email}
         </Text>
       </div>
