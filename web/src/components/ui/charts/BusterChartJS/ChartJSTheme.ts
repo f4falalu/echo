ef7bb599a7b7232f1e7e85d6de96c17007e5c4f5
Nate Kelley <nate@buster.so>
2025-05-08 09:20:02 -0600

    Update ChartJSTheme.ts

--- web/src/components/ui/charts/BusterChartJS/ChartJSTheme.ts
+++ web/src/components/ui/charts/BusterChartJS/ChartJSTheme.ts
@@ -86,11 +86,11 @@ ChartJS.defaults.font = {
 };
 
 [
-  (ChartJS.defaults.scales.category,
+  ChartJS.defaults.scales.category,
   ChartJS.defaults.scales.linear,
   ChartJS.defaults.scales.logarithmic,
   ChartJS.defaults.scales.time,
-  ChartJS.defaults.scales.timeseries)
+  ChartJS.defaults.scales.timeseries
 ].forEach((scale) => {
   scale.title = {
     ...scale.title,
