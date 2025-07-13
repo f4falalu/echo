d7142b838c4faa81a5f5dd2d4686edac4a88712e
Nate Kelley <nate@buster.so>
2025-07-13 08:17:16 -0600

    optimistically grab new chats

--- apps/web/src/api/buster-electric/messages/hooks.ts
+++ apps/web/src/api/buster-electric/messages/hooks.ts
@@ -4,7 +4,7 @@ import { useShape, useShapeStream } from '../instances';
 import { useChatUpdate } from '@/context/Chats/useChatUpdate';
 import { updateMessageShapeToIChatMessage } from './helpers';
 import { useMemoizedFn } from '@/hooks';
-import { useGetChatMemoized } from '@/api/buster_rest/chats';
+import { prefetchGetListChats, useGetChatMemoized } from '@/api/buster_rest/chats';
 import uniq from 'lodash/uniq';
 
 export const useGetMessage = ({ chatId, messageId }: { chatId: string; messageId: string }) => {
@@ -33,6 +33,7 @@ export const useTrackAndUpdateMessageChanges = (
 ) => {
   const { onUpdateChatMessage, onUpdateChat } = useChatUpdate();
   const getChatMemoized = useGetChatMemoized();
+  
   const shape = useMemo(
     () => messageShape({ chatId: chatId || '', messageId }),
     [chatId, messageId]
@@ -57,6 +58,10 @@ export const useTrackAndUpdateMessageChanges = (
               ...chat,
               message_ids: allMessageIds
             });
+          }
+
+          if(iChatMessage.is_completed){
+            prefetchGetListChats()
           }
         }
         callback?.(iChatMessage);
