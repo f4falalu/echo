5c15664b224d864c5a391435a1ed4655215c774a
Nate Kelley <nate@buster.so>
2025-07-17 09:51:48 -0600

    Add new column and rules around type safety

---
+++ packages/database/src/schema-types/organization.ts
@@ -0,0 +1,7 @@
+// Organization Color Palette Types
+export type OrganizationColorPalette = {
+  id: string;
+  color: string[];
+};
+
+export type OrganizationColorPalettes = OrganizationColorPalette[];
