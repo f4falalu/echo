d66f5d1b63bb7bf63a7f3107a12cfad43d09cec2
Nate Kelley <nate@buster.so>
2025-07-18 13:10:21 -0600

    Update usePalettes.ts

--- apps/web/src/context-hooks/usePalettes.ts
+++ apps/web/src/context-hooks/usePalettes.ts
@@ -3,7 +3,6 @@ import type { ColorPalette } from '@buster/server-shared/organization';
 import { useMemo } from 'react';
 import { useColorDictionaryThemes } from '../api/buster_rest/dictionaries';
 import { DEFAULT_CHART_THEME } from '@buster/server-shared/metrics';
-import { useGetCurrencies } from '../api/buster_rest/dictionaries';
 
 const useGetOrganizationPalettes = () => {
   const { data: userData } = useGetMyUserInfo();
@@ -30,11 +29,11 @@ export const useGetPalettes = () => {
   const { organizationPalettes, selectedPaletteId } = useGetOrganizationPalettes();
   const { data: dictionaryPalettes, isError: isErrorDictionaryPalettes } =
     useColorDictionaryThemes();
-  const { data: currencies } = useGetCurrencies();
 
   return useMemo(() => {
     const allPalettes = [...dictionaryPalettes, ...organizationPalettes];
-    const defaultPalette = allPalettes.find((palette) => palette.id === selectedPaletteId);
+    const defaultPalette =
+      allPalettes.find((palette) => palette.id === selectedPaletteId) || dictionaryPalettes[0];
 
     return {
       allPalettes,
@@ -42,17 +41,16 @@ export const useGetPalettes = () => {
       dictionaryPalettes,
       selectedPaletteId,
       defaultPalette,
-
       isErrorDictionaryPalettes
     };
   }, [dictionaryPalettes, organizationPalettes, selectedPaletteId]);
 };
 
 export const useSelectedColorPalette = (colors: string[] | undefined | null): string[] => {
-  const { defaultOrganizationPalette } = useGetOrganizationPalettes();
+  const { defaultPalette } = useGetPalettes();
 
   return useMemo(
-    () => colors || defaultOrganizationPalette?.colors || DEFAULT_CHART_THEME,
-    [colors, defaultOrganizationPalette]
+    () => colors || defaultPalette?.colors || DEFAULT_CHART_THEME,
+    [colors, defaultPalette]
   );
 };
