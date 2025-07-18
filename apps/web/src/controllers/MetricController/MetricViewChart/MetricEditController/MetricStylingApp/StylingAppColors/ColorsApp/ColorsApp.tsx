ea0048e8e23e65ece6cc24a41d04ef3940d58b48
Nate Kelley <nate@buster.so>
2025-07-17 22:51:26 -0600

    Update color apps to include custom themes

--- apps/web/src/controllers/MetricController/MetricViewChart/MetricEditController/MetricStylingApp/StylingAppColors/ColorsApp/ColorsApp.tsx
+++ apps/web/src/controllers/MetricController/MetricViewChart/MetricEditController/MetricStylingApp/StylingAppColors/ColorsApp/ColorsApp.tsx
@@ -5,39 +5,42 @@ import { useMemoizedFn } from '@/hooks';
 import type { IColorTheme } from '@/components/features/colors/ThemeList';
 import { ThemeList } from '@/components/features/colors/ThemeList';
 import { useColorThemes } from '@/api/buster_rest/dictionaries';
+import { useGetMyUserInfo, useGetUser } from '@/api/buster_rest/users';
 
 export const ColorsApp: React.FC<{
   colors: ChartConfigProps['colors'];
   onUpdateChartConfig: (chartConfig: Partial<ChartConfigProps>) => void;
 }> = ({ colors, onUpdateChartConfig }) => {
   const { data: themes } = useColorThemes();
+  const { data: userConfig } = useGetMyUserInfo();
+
+  const organizationPalettes =
+    userConfig?.organizations?.[0]?.organizationColorPalettes?.palettes || [];
+
+  const iThemes: Required<IColorTheme>[] = useMemo(() => {
+    const organizationAndDefaultThemes = [...(organizationPalettes || []), ...themes];
+
+    return organizationAndDefaultThemes.map((theme) => ({
+      ...theme,
+      selected: isEqual(theme.colors, colors)
+    }));
+  }, [themes, organizationPalettes, colors]);
+
   const onChangeColorTheme = useMemoizedFn((theme: IColorTheme) => {
     onUpdateChartConfig({ colors: theme.colors });
   });
 
   return (
     <div className="flex flex-col space-y-2">
-      <ColorPicker
-        selectedSegmentColors={themes}
-        colors={colors}
-        onChangeColorTheme={onChangeColorTheme}
-      />
+      <ColorPicker themes={iThemes} onChangeColorTheme={onChangeColorTheme} />
     </div>
   );
 };
 
 const ColorPicker: React.FC<{
-  selectedSegmentColors: IColorTheme[];
-  colors: ChartConfigProps['colors'];
+  themes: Required<IColorTheme>[];
   onChangeColorTheme: (theme: IColorTheme) => void;
-}> = React.memo(({ selectedSegmentColors, colors, onChangeColorTheme }) => {
-  const themes = useMemo(() => {
-    return selectedSegmentColors.map((theme) => ({
-      ...theme,
-      selected: isEqual(theme.colors, colors)
-    }));
-  }, [selectedSegmentColors, colors]);
-
+}> = React.memo(({ themes, onChangeColorTheme }) => {
   return <ThemeList themes={themes} onChangeColorTheme={onChangeColorTheme} />;
 });
 ColorPicker.displayName = 'ColorPicker';
