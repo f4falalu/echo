95f4f52372e3753c137c2bfa6c58eaf13533c24a
dal <dallin@buster.so>
2025-07-21 15:54:19 -0600

    changes for ci/cd with the sandbox code

--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -1,3 +1,4 @@
 export { runTypescript } from './execute/run-typescript';
 export { createSandbox } from './management/create-sandbox';
 export type { RunTypeScriptOptions, CodeRunResponse } from './execute/run-typescript';
+export type { Sandbox } from '@daytonaio/sdk';
