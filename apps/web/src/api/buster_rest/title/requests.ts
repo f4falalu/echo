1d646c003d7771db2213bb46ed928bc703333ed3
Nate Kelley <nate@buster.so>
2025-07-24 18:15:43 -0600

    Update request to use v2

--- apps/web/src/api/buster_rest/title/requests.ts
+++ apps/web/src/api/buster_rest/title/requests.ts
@@ -1,9 +1,11 @@
 import { serverFetch } from '../../createServerInstance';
 import { GetTitleResponse, GetTitleRequest } from '@buster/server-shared/title';
+import { BASE_URL_V2 } from '../config';
 
 export const getTitle_server = async (params: GetTitleRequest) => {
   return serverFetch<GetTitleResponse>(`/title`, {
     method: 'GET',
-    params
+    params,
+    baseURL: BASE_URL_V2
   });
 };
