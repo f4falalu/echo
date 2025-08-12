66623fb2a57f5a1b78a4fb4e1efe6d48274b3ed4
Nate Kelley <nate@buster.so>
2025-08-12 17:05:02 -0600

    route guard the index

--- apps/web-tss/src/routes/index.tsx
+++ apps/web-tss/src/routes/index.tsx
@@ -1,11 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import logo from '../logo.svg';
 
 export const Route = createFileRoute('/')({
   component: App,
   beforeLoad: async ({ context }) => {
     const hasUser = context.user;
-    console.log('hasUser', hasUser);
+    const isAnonymous = context.user?.is_anonymous;
+    if (!hasUser || isAnonymous) {
+      throw redirect({ to: '/auth/login' });
+    }
   },
 });
 
