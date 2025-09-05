820fbe91da2083760694a814cabb3c3db26f5908
Nate Kelley <nate@buster.so>
2025-09-05 09:55:29 -0600

    update package versions

--- apps/web/src/components/ui/charts/BusterChartJS/core/typedCharts.tsx
+++ apps/web/src/components/ui/charts/BusterChartJS/core/typedCharts.tsx
@@ -1,4 +1,4 @@
-import type { ChartComponentLike, ChartType } from "chart.js";
+import type { ChartComponentLike, ChartType } from 'chart.js';
 import {
   BarController,
   BubbleController,
@@ -9,52 +9,33 @@ import {
   PolarAreaController,
   RadarController,
   ScatterController,
-} from "chart.js";
-import { forwardRef } from "react";
-import { Chart } from "./chart.js";
-import type {
-  ChartJSOrUndefined,
-  ChartProps,
-  TypedChartComponent,
-} from "./types.js";
+} from 'chart.js';
+import { forwardRef } from 'react';
+import { Chart } from './chart.js';
+import type { ChartJSOrUndefined, ChartProps, TypedChartComponent } from './types.js';
 
-function createTypedChart<T extends ChartType>(
-  type: T,
-  registerables: ChartComponentLike
-) {
+function createTypedChart<T extends ChartType>(type: T, registerables: ChartComponentLike) {
   ChartJS.register(registerables);
-  const myRef = forwardRef<ChartJSOrUndefined<T>, Omit<ChartProps<T>, "type">>(
-    (props, ref) => <Chart {...props} ref={ref} type={type} />
-  );
+  const myRef = forwardRef<ChartJSOrUndefined<T>, Omit<ChartProps<T>, 'type'>>((props, ref) => (
+    <Chart {...props} ref={ref} type={type} />
+  ));
   myRef.displayName = `Chart${type}`;
 
   return myRef as TypedChartComponent<T>;
 }
 
-export const Line = /* #__PURE__ */ createTypedChart("line", LineController);
+export const Line = /* #__PURE__ */ createTypedChart('line', LineController);
 
-export const Bar = /* #__PURE__ */ createTypedChart("bar", BarController);
+export const Bar = /* #__PURE__ */ createTypedChart('bar', BarController);
 
-export const Radar = /* #__PURE__ */ createTypedChart("radar", RadarController);
+export const Radar = /* #__PURE__ */ createTypedChart('radar', RadarController);
 
-export const Doughnut = /* #__PURE__ */ createTypedChart(
-  "doughnut",
-  DoughnutController
-);
+export const Doughnut = /* #__PURE__ */ createTypedChart('doughnut', DoughnutController);
 
-export const PolarArea = /* #__PURE__ */ createTypedChart(
-  "polarArea",
-  PolarAreaController
-);
+export const PolarArea = /* #__PURE__ */ createTypedChart('polarArea', PolarAreaController);
 
-export const Bubble = /* #__PURE__ */ createTypedChart(
-  "bubble",
-  BubbleController
-);
+export const Bubble = /* #__PURE__ */ createTypedChart('bubble', BubbleController);
 
-export const Pie = /* #__PURE__ */ createTypedChart("pie", PieController);
+export const Pie = /* #__PURE__ */ createTypedChart('pie', PieController);
 
-export const Scatter = /* #__PURE__ */ createTypedChart(
-  "scatter",
-  ScatterController
-);
+export const Scatter = /* #__PURE__ */ createTypedChart('scatter', ScatterController);
