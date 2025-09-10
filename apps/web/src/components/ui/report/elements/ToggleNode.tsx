68eaacea67bb933d68763ea2efc620fd2bd0d805
Nate Kelley <nate@buster.so>
2025-09-09 21:43:08 -0600

    Update toggle kit

--- apps/web/src/components/ui/report/elements/ToggleNode.tsx
+++ apps/web/src/components/ui/report/elements/ToggleNode.tsx
@@ -1,8 +1,6 @@
 import { useToggleButton, useToggleButtonState } from '@platejs/toggle/react';
-
 import type { PlateElementProps } from 'platejs/react';
 import { PlateElement } from 'platejs/react';
-import * as React from 'react';
 import { Button } from '@/components/ui/buttons';
 import { ChevronRight } from '@/components/ui/icons';
 import { cn } from '@/lib/classMerge';
@@ -15,16 +13,17 @@ export function ToggleElement(props: PlateElementProps) {
   return (
     <PlateElement {...props} className="pl-6">
       <Button
+        size="small"
         variant="ghost"
-        className="text-muted-foreground hover:bg-accent absolute top-0 -left-0.5 size-6 cursor-pointer items-center justify-center rounded-md p-px transition-colors select-none [&_svg]:size-4"
+        className="absolute top-0 -left-0.5 size-6 cursor-pointer items-center justify-center rounded-md p-px text-muted-foreground transition-colors select-none hover:bg-accent [&_svg]:size-4"
         contentEditable={false}
         {...buttonProps}
         prefix={
-          <div className={cn('transition-transform duration-75', open ? 'rotate-90' : 'rotate-0')}>
+          <div className={cn('transition-transform duration-100', open ? 'rotate-90' : 'rotate-0')}>
             <ChevronRight />
           </div>
         }
-      ></Button>
+      />
       {props.children}
     </PlateElement>
   );
