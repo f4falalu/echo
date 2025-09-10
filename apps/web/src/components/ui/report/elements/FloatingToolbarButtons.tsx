4a386590f49bfb337259c5747c962742b6687fb0
Nate Kelley <nate@buster.so>
2025-09-09 20:51:47 -0600

    float toolbars

--- apps/web/src/components/ui/report/elements/FloatingToolbarButtons.tsx
+++ apps/web/src/components/ui/report/elements/FloatingToolbarButtons.tsx
@@ -1,17 +1,11 @@
 import { KEYS } from 'platejs';
 import { useEditorReadOnly } from 'platejs/react';
-import * as React from 'react';
 import { ToolbarGroup } from '@/components/ui/toolbar/Toolbar';
 import { NodeTypeIcons } from '../config/icons';
 import { createLabel } from '../config/labels';
-import { AIToolbarButton } from './AIToolbarButton';
-import { CommentToolbarButton } from './CommandToolbarButton';
-// import { InlineEquationToolbarButton } from './EquationToolbarButton';
 import { LinkToolbarButton } from './LinkToolbarButton';
 import { MarkToolbarButton } from './MarktoolbarButton';
 import { MoreToolbarButton } from './MoreToolbarButton';
-// import { MoreToolbarButton } from './MoreToolbarButton';
-import { SuggestionToolbarButton } from './SuggestionToolbarButton';
 import { TurnIntoToolbarButton } from './TurnIntoToolbarButton';
 
 export function FloatingToolbarButtons() {
@@ -20,42 +14,33 @@ export function FloatingToolbarButtons() {
   return (
     <>
       {!readOnly && (
-        <>
-          {/* <ToolbarGroup>
-            <AIToolbarButton tooltip="AI commands">
-              {AIIcon}
-              Ask AI
-            </AIToolbarButton>
-          </ToolbarGroup> */}
+        <ToolbarGroup>
+          <TurnIntoToolbarButton />
 
-          <ToolbarGroup>
-            <TurnIntoToolbarButton />
+          <MarkToolbarButton nodeType={KEYS.bold} tooltip={createLabel('bold')}>
+            <NodeTypeIcons.bold />
+          </MarkToolbarButton>
 
-            <MarkToolbarButton nodeType={KEYS.bold} tooltip={createLabel('bold')}>
-              <NodeTypeIcons.bold />
-            </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.italic} tooltip={createLabel('italic')}>
+            <NodeTypeIcons.italic />
+          </MarkToolbarButton>
 
-            <MarkToolbarButton nodeType={KEYS.italic} tooltip={createLabel('italic')}>
-              <NodeTypeIcons.italic />
-            </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.underline} tooltip={createLabel('underline')}>
+            <NodeTypeIcons.underline />
+          </MarkToolbarButton>
 
-            <MarkToolbarButton nodeType={KEYS.underline} tooltip={createLabel('underline')}>
-              <NodeTypeIcons.underline />
-            </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.strikethrough} tooltip={createLabel('strikethrough')}>
+            <NodeTypeIcons.strikethrough />
+          </MarkToolbarButton>
 
-            <MarkToolbarButton nodeType={KEYS.strikethrough} tooltip={createLabel('strikethrough')}>
-              <NodeTypeIcons.strikethrough />
-            </MarkToolbarButton>
+          <MarkToolbarButton nodeType={KEYS.code} tooltip={createLabel('code')}>
+            <NodeTypeIcons.code />
+          </MarkToolbarButton>
 
-            <MarkToolbarButton nodeType={KEYS.code} tooltip={createLabel('code')}>
-              <NodeTypeIcons.code />
-            </MarkToolbarButton>
+          {/* <InlineEquationToolbarButton /> */}
 
-            {/* <InlineEquationToolbarButton /> */}
-
-            <LinkToolbarButton />
-          </ToolbarGroup>
-        </>
+          <LinkToolbarButton />
+        </ToolbarGroup>
       )}
 
       <ToolbarGroup>
