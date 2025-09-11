440489d8be49271cc843eb8609cc0fb20cc11c2c
Wells Bunker <wells@buster.so>
2025-09-10 23:07:51 -0600

    update migrations

--- packages/database/drizzle/0093_silly_wiccan.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "suggested_prompts" jsonb DEFAULT '{
-        "suggestedPrompts": {
-          "report": [],
-          "dashboard": [],
-          "visualization": [],
-          "help": []
-        },
-        "updatedAt": null
-      }'::jsonb NOT NULL;
