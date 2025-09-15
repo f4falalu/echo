4c61f5f9d732dd80af22270419909a2b354c2de4
Nate Kelley <nate@buster.so>
2025-09-15 12:56:13 -0600

    edit mode option

--- apps/web/src/context/BusterAssets/metric-server/metricChartServerAssetContext.tsx
+++ apps/web/src/context/BusterAssets/metric-server/metricChartServerAssetContext.tsx
@@ -1,11 +1,14 @@
-import { ClientOnly, Outlet } from '@tanstack/react-router';
-import { lazy, Suspense, useRef } from 'react';
+import { ClientOnly, Outlet, useLocation, useNavigate, useSearch } from '@tanstack/react-router';
+import { lazy, Suspense, useRef, useTransition } from 'react';
+import { z } from 'zod';
 import { AppSplitter, type LayoutSize } from '@/components/ui/layouts/AppSplitter';
 import { useGetMetricParams } from '@/context/Metrics/useGetMetricParams';
 import { MetricViewChartController } from '@/controllers/MetricController/MetricViewChartController';
+import { useMount } from '@/hooks/useMount';
 import {
   useIsMetricEditMode,
   useMetricEditSplitter,
+  useMetricEditToggle,
 } from '@/layouts/AssetContainer/MetricAssetContainer/MetricContextProvider';
 import { CircleSpinnerLoaderContainer } from '../../../components/ui/loaders';
 
@@ -13,14 +16,41 @@ const defaultLayoutClosed: LayoutSize = ['auto', '0px'];
 const defaultLayoutOpen: LayoutSize = ['auto', '300px'];
 const autoSaveId = `metric-chart-layout`;
 
+export const validateSearch = z.object({
+  editMode: z.boolean().optional(),
+});
+
 export const component = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const { metricId, metricVersionNumber } = useGetMetricParams();
+  const editMode = useSearch({
+    strict: false,
+    select: (v) => v.editMode ?? false,
+  });
+
   const isMetricEditMode = useIsMetricEditMode();
   const splitterRef = useMetricEditSplitter();
+  const toggleEditMode = useMetricEditToggle();
 
-  const hasSeenMetricEditMode = useRef(false);
+  const hasSeenMetricEditMode = useRef(editMode);
 
   const defaultLayout = isMetricEditMode ? defaultLayoutOpen : defaultLayoutClosed;
+
+  useMount(() => {
+    if (editMode) {
+      setTimeout(() => {
+        navigate({
+          to: location.pathname,
+          search: {},
+          replace: true,
+        }).then(() => {
+          toggleEditMode(true);
+        });
+      }, 250);
+    }
+  });
 
   if (isMetricEditMode && !hasSeenMetricEditMode.current) {
     hasSeenMetricEditMode.current = true;
@@ -56,6 +86,7 @@ const MetricEditController = lazy(() =>
     })
   )
 );
+
 const RightChildren = ({ metricId, renderChart }: { metricId: string; renderChart: boolean }) => {
   return renderChart ? (
     <Suspense fallback={<CircleSpinnerLoaderContainer />}>
