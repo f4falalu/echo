f35506275a092dc0b9e0fa01027d1dabdbfaac83
Nate Kelley <nate@buster.so>
2025-09-29 18:25:27 -0600

    Update seperator to use css

--- apps/web/src/components/ui/inputs/MentionInputSuggestions/MentionInputSuggestionsSeparator.tsx
+++ apps/web/src/components/ui/inputs/MentionInputSuggestions/MentionInputSuggestionsSeparator.tsx
@@ -1,16 +1,29 @@
-import { Command, useCommandState } from 'cmdk';
+import { useCommandState } from 'cmdk';
 import type React from 'react';
 import { cn } from '@/lib/utils';
 
 export const MentionInputSuggestionsSeparator = ({
   children,
+  className,
   ...props
-}: React.ComponentPropsWithoutRef<typeof Command.Separator>) => {
-  const hasResults = useCommandState((x) => x);
-  console.log(hasResults);
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const hasResults = useCommandState((x) => x.filtered.count) > 0;
   return (
-    <Command.Separator className={cn('bg-border -mx-1 h-px my-1.5', props.className)} {...props}>
+    <div
+      className={cn(
+        'bg-border -mx-1 h-px my-1.5',
+        // Hide if first child
+        'first:hidden',
+        // Hide if last child
+        'last:hidden',
+        // Hide if next sibling is another separator
+        'has-[+[role="separator"]]:hidden',
+        !hasResults && 'hidden',
+        className
+      )}
+      {...props}
+    >
       {children}
-    </Command.Separator>
+    </div>
   );
 };
