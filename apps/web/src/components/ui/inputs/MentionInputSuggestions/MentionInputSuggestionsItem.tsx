127dafbbd365db20fff44aede08d8b07735e041f
Nate Kelley <nate@buster.so>
2025-09-30 11:42:03 -0600

    Add a height

--- apps/web/src/components/ui/inputs/MentionInputSuggestions/MentionInputSuggestionsItem.tsx
+++ apps/web/src/components/ui/inputs/MentionInputSuggestions/MentionInputSuggestionsItem.tsx
@@ -36,7 +36,7 @@ export const MentionInputSuggestionsItem = ({
         className={cn(
           'data-[selected=true]:bg-item-hover data-[selected=true]:text-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-base outline-none select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
           !disabled ? 'cursor-pointer' : 'cursor-not-allowed',
-          'text-secondary group',
+          'text-secondary group min-h-9',
           className
         )}
         value={value}
