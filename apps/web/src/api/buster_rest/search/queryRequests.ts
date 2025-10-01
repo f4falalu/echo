ffde4ef59c9affed84797cd08b5c48c7b28e5863
Nate Kelley <nate@buster.so>
2025-10-01 15:55:58 -0600

    kill query if another one comes in

--- apps/web/src/api/buster_rest/search/queryRequests.ts
+++ apps/web/src/api/buster_rest/search/queryRequests.ts
@@ -6,15 +6,11 @@ import { search } from './requests';
 
 export const useSearch = <T = SearchTextResponse>(
   params: Parameters<typeof search>[0],
-  options?: Omit<UseQueryOptions<SearchTextResponse, ApiError, T>, 'queryKey' | 'queryFn'>,
-  postQueryOptions?: {
-    doNotUnwrapData?: boolean;
-  }
+  options?: Omit<UseQueryOptions<SearchTextResponse, ApiError, T>, 'queryKey' | 'queryFn'>
 ) => {
-  const { doNotUnwrapData = false } = postQueryOptions || {};
   return useQuery<SearchTextResponse, ApiError, T>({
     ...searchQueryKeys.getSearchResult(params),
-    queryFn: () => search(params),
+    queryFn: ({ signal }) => search(params, signal),
     select: options?.select,
     ...options,
     placeholderData: keepPreviousData,
